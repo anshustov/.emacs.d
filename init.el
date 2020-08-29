57970fcea30df454fa3d5254fb680961ea758506
anshustov <anshustov@gmail.com>
2020-08-29 22:28:08 +0300

    bug fix

--- init.el
+++ init.el
@@ -8,7 +8,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-bullets elpy auctex counsel swiper ivy smartparens ace-jump-mode htmlize magit web-mode js2-mode php-mode pug-mode zoom emmet-mode markdown-mode indent-guide company yasnippet-snippets yasnippet multiple-cursors projectile exec-path-from-shell doom-modeline srcery-theme use-package)))
+   (quote
+    (org-bullets elpy auctex counsel swiper ivy smartparens ace-jump-mode htmlize magit web-mode js2-mode php-mode pug-mode zoom emmet-mode markdown-mode indent-guide company yasnippet-snippets yasnippet multiple-cursors projectile exec-path-from-shell doom-modeline srcery-theme use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
