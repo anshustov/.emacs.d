8d0860fd6db6dd72236d389eb07538611a225903
Aleksey Shustov <anshustov@gmail.com>
2021-05-20 22:25:48 +0300

    sync

--- init.el
+++ init.el
@@ -2,3 +2,20 @@
 (org-babel-load-file
  (expand-file-name "README.org"
                    user-emacs-directory))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+	 '(tex zoom yasnippet-snippets web-mode use-package undo-tree srcery-theme smartparens rainbow-delimiters php-mode pdf-tools pass org-bullets multiple-cursors magit lsp-ui lsp-ivy latex-preview-pane js2-mode indent-guide htmlize go-mode flycheck exec-path-from-shell emmet-mode elpy doom-modeline dimmer counsel-projectile auctex all-the-icons-dired aggressive-indent ace-window ace-jump-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.1))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0)))))
